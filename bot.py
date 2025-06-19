4b81c62b962f6d207c459fd09d1272165cd9db44
flsp <florian.lesp@gmail.com>
2025-06-19 10:09:36 +0200

    feat: send message when user moves in the channel

--- bot.py
+++ bot.py
@@ -6,15 +6,16 @@ from discord.ext import commands
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
+channel_id = int(os.getenv("CHANNEL_1_ID"))
 
-print(f"Token lu : {token}") 
 
 intents = discord.Intents.default()
-intents.message_content = True 
 intents.voice_states = True
+intents.message_content = True  # utile plus tard
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents = intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -24,6 +25,21 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    print(
+        f"******************\n Channel précédent: {before.channel} \n Channel suivant: {after.channel} \n******************"
+    )
+
+    # Vérification que l'user n'est pas parti et qu'il a bien changé de channel.
+    if after.channel is not None and before.channel != after.channel:
+        if after.channel.id == channel_id:
+            # Récupère le salon de l'utilisateur Discord dans le serveur à partir de son ID
+            text_channel = member.guild.get_channel(channel_id)
+            if text_channel:
+                await text_channel.send(f"{member.display_name} est arrivé dans Work !")
 
 
 bot.run(token)
