17f201c79937dd6163496c0bd8db0075ddba245b
flsp <florian.lesp@gmail.com>
2025-06-24 08:19:47 +0200

    feat: create dedicated file for events

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from commands import register_commands
+from events import register_events
 
 
 load_dotenv()
@@ -16,31 +18,11 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-from commands import register_commands
 
 register_commands(bot)
 
 
-@bot.event
-async def on_ready():
-    print(f"Connecté en tant que {bot.user.name}")
-
-
-@bot.event
-async def on_voice_state_update(member, before, after):
-    print(
-        f"******************\n Channel précédent: {before.channel} \n Channel suivant: {after.channel} \n******************"
-    )
-
-    # Vérification que l'user n'est pas parti et qu'il a bien changé de channel.
-    if after.channel is not None and before.channel != after.channel:
-        if after.channel.id == channel_id:
-            # Récupère le salon de l'utilisateur Discord dans le serveur à partir de son ID
-            text_channel = member.guild.get_channel(channel_id)
-            if text_channel:
-                await text_channel.send(
-                    f"Yooo!! {member.mention} est la pour bosser ! \n Fais moi signe quand tu es prêt à commencer ! (`!start` ou `!help` pour afficher les commandes)"
-                )
+register_events(bot, channel_id)
 
 
 bot.run(token)
---
+++ events.py
@@ -0,0 +1,20 @@
+def register_events(bot, channel_id):
+    @bot.event
+    async def on_ready():
+        print(f"Connecté en tant que {bot.user.name}")
+
+    @bot.event
+    async def on_voice_state_update(member, before, after):
+        print(
+            f"******************\n Channel précédent: {before.channel} \n Channel suivant: {after.channel} \n******************"
+        )
+
+        # Vérification que l'user n'est pas parti et qu'il a bien changé de channel.
+        if after.channel is not None and before.channel != after.channel:
+            if after.channel.id == channel_id:
+                # Récupère le salon de l'utilisateur Discord dans le serveur à partir de son ID
+                text_channel = member.guild.get_channel(channel_id)
+                if text_channel:
+                    await text_channel.send(
+                        f"Yooo!! {member.mention} est la pour bosser ! \n Fais moi signe quand tu es prêt à commencer ! (`!start` ou `!help` pour afficher les commandes)"
+                    )
